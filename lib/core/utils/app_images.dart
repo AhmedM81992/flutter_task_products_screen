240fc894baca4898964fd499635ef87b23a8f533
Ahmed <ahmedm.81992@gmail.com>
2024-07-14 13:59:45 +0300

    added icons(images)

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String logo = "assets/images/icons/logo.svg";
+  static const String star = "assets/images/icons/star.svg";
+  static const String fav = "assets/images/icons/fav.svg";
+  static const String search = "assets/images/icons/search.svg";
+  static const String cart = "assets/images/icons/cart.svg";
+}
